4f9c28b01aea30055232f28477f93f4ff15082de
ScrewThisNoise <foreveryounghentai@gmail.com>
2022-01-27 20:07:38 +0100

    Added Models for Progress and Files

---
+++ ZipmodHelper/FileModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipmodHelper
+{
+    public class FileModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string NewName { get; set; }
+        public string Path { get; set; }
+        public string MD5 { get; set; }
+        public string[] Game { get; set; }
+    }
+}
---
+++ ZipmodHelper/ProgressModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipmodHelper
+{
+    public class ProgressModel
+    {
+        public int PercentageComplete { get; set; } = 0;
+        public List<FileModel> FilesProcessed { get; set; } = new List<FileModel>();
+    }
+}
