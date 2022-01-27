843e599a7f8dd1c2c1c5a3bfd3011b7c1b9151cb
ScrewThisNoise <foreveryounghentai@gmail.com>
2022-01-27 08:08:03 +0100

    Added DoWork

---
+++ ZipmodHelper/DoWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipmodHelper
+{
+    internal class DoWork
+    {
+        public static void Start(string inputFolder, string outputFolder, string tempFolder)
+        {
+
+        }
+    }
+}
